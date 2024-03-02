3322e22edc2886d77f7f8c4526aeb1492b45a432
Elizur <elisurfz@gmail.com>
2024-03-02 14:19:59 +0300

    add http client

---
+++ http_client/http_client.go
@@ -0,0 +1,31 @@
+package httpclient
+
+import "net/http"
+
+type HTTPClient struct {
+	client http.Client
+}
+
+func CreateClient() HTTPClient {
+	return HTTPClient{client: http.Client{}}
+}
+
+func (client HTTPClient) CreateNewRequest(method, url string, header map[string]string, body []byte) (http.Request, error) {
+	var req *http.Request
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return *req, err
+	}
+	for key, value := range header {
+		req.Header.Set(key, value)
+	}
+	return *req, nil
+}
+
+func (client HTTPClient) DoRequest(req http.Request) (*http.Response, error) {
+	res, err := client.client.Do(&req)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
