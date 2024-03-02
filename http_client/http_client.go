de9853df62276c781f53f0351e1020584ea8daee
Elizur <elisurfz@gmail.com>
2024-03-02 23:41:37 +0300

    use reference

--- http_client/http_client.go
+++ http_client/http_client.go
@@ -3,27 +3,26 @@ package httpclient
 import "net/http"
 
 type HTTPClient struct {
-	client http.Client
+	client *http.Client
 }
 
-func CreateClient() HTTPClient {
-	return HTTPClient{client: http.Client{}}
+func CreateClient() *HTTPClient {
+	return &HTTPClient{client: &http.Client{}}
 }
 
-func (client HTTPClient) CreateNewRequest(method, url string, header map[string]string, body []byte) (http.Request, error) {
-	var req *http.Request
+func (client *HTTPClient) CreateNewRequest(method, url string, header map[string]string, body []byte) (*http.Request, error) {
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
-		return *req, err
+		return req, err
 	}
 	for key, value := range header {
 		req.Header.Set(key, value)
 	}
-	return *req, nil
+	return req, nil
 }
 
-func (client HTTPClient) DoRequest(req http.Request) (*http.Response, error) {
-	res, err := client.client.Do(&req)
+func (client *HTTPClient) DoRequest(req *http.Request) (*http.Response, error) {
+	res, err := client.client.Do(req)
 	if err != nil {
 		return nil, err
 	}
